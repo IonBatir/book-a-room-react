61ff671eeeb4940f52e1f13abe94c99071b03c39
Ion Batîr <ionbatir@gmail.com>
2019-01-08 19:41:24 +0200

    Refactor api requests

--- src/api.js
+++ src/api.js
@@ -2,14 +2,26 @@ import axios from "axios";
 
 const url = "https://book-a-room.ionbatir.com/api/";
 
-export const getHotels = () =>
+export const fetchItems = endpoint =>
   axios
-    .get(url + "hotel")
+    .get(url + endpoint)
     .then(response => Promise.resolve(response.data))
     .catch(error => Promise.reject(error));
 
-export const deleteHotel = id =>
+export const updateItem = (endpoint, data) =>
   axios
-    .delete(url + "hotel", { params: { id } })
+    .put(url + endpoint, data)
+    .then(response => Promise.resolve(response.data))
+    .catch(error => Promise.reject(error));
+
+export const addItem = (endpoint, data) =>
+  axios
+    .post(url + endpoint, data)
+    .then(response => Promise.resolve(response.data))
+    .catch(error => Promise.reject(error));
+
+export const deleteItem = (endpoint, id) =>
+  axios
+    .delete(url + endpoint, { params: { id } })
     .then(response => Promise.resolve(response.data))
     .catch(error => Promise.reject(error));
