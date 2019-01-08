6d6c0dbaec8fbc67a1b64482cd40e24ab6a0ee12
Ion Batîr <ionbatir@gmail.com>
2019-01-08 12:51:08 +0200

    Add hotel requests

--- src/api.js
+++ src/api.js
@@ -1,9 +1,15 @@
 import axios from "axios";
 
-const api = "https://book-a-room.ionbatir.com/api/";
+const url = "https://book-a-room.ionbatir.com/api/";
 
 export const getHotels = () =>
   axios
-    .get(api + "hotel")
+    .get(url + "hotel")
+    .then(response => Promise.resolve(response.data))
+    .catch(error => Promise.reject(error));
+
+export const deleteHotel = id =>
+  axios
+    .delete(url + "hotel", { params: { id } })
     .then(response => Promise.resolve(response.data))
     .catch(error => Promise.reject(error));
